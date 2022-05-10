3e0245559ba39100f380ff329226a172db713061
Kian Ryan <kian@orangetentacle.co.uk>
2022-05-10 16:23:18 +0100

    Initial commit

---
+++ wp-atom.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=httpwww.catashton.co.uk/feed/atom/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "httpwww.catashton.co.uk/feed/atom/";
+		</script>
+
+		<p>You are being redirected to <a href="httpwww.catashton.co.uk/feed/atom/">httpwww.catashton.co.uk/feed/atom/</a></p>
+	</body>
+</html>
